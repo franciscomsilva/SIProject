8e030ae3bba4514576e678c9ea236a55984d989f
Francisco Silva <franciscosilva800@gmail.com>
2019-12-06 12:16:11 +0000

    Model changes

--- IPL1920-IS-IPLeiriaSmartCampus/Models/User.cs
+++ IPL1920-IS-IPLeiriaSmartCampus/Models/User.cs
@@ -13,28 +13,6 @@ namespace Models
         public string Name { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
-        public User(string name, string password)
-        {
-            this.Id = -1;
-            this.Name = name;
-            this.Password = password;
-        }
-        public User(int id, string name, string password)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.Password = password;
-        }
-        public User()
-        {
-
-        }
-        public User(int id, string name, string password, bool isAdmin)
-        {
-            this.Id = id;
-            this.Name = name;
-            this.Password = password;
-            this.IsAdmin = isAdmin;
-        }
+    
     }
 }
