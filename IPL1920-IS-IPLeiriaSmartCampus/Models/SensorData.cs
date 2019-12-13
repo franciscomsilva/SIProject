5d4895309a2b7bb5087b7dee1d450809e3735fca
ADMIN <ricardosoares1999@hotmail.com>
2019-12-13 11:54:19 +0000

    Added Sensor Data Model

---
+++ IPL1920-IS-IPLeiriaSmartCampus/Models/SensorData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class SensorData
+    {
+        public int sensor_id { get; set; }
+        public int location_id { get; set; }
+        public string measure_name { get; set; }
+        public string measure_type { get; set; }
+        public string value { get; set; }
+    }
+}
