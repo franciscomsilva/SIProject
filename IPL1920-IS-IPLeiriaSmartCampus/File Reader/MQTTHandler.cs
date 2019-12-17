2df1146619a1727770b81e2f0d986da58d6bcf28
Francisco Silva <franciscosilva800@gmail.com>
2019-12-17 17:24:14 +0000

    Added MQTTHandler

---
+++ IPL1920-IS-IPLeiriaSmartCampus/File Reader/MQTTHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+using Newtonsoft.Json;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace File_Reader
+{
+    class MQTTHandler
+    {
+        private MqttClient mClient = null;
+        public List<string> topics = new List<string>();
+        private static MQTTHandler instance = null;
+     
+
+        private MQTTHandler()
+        {
+            this.topics.Add("alerts/login");
+            this.topics.Add("alerts/readingType");
+            this.topics.Add("alerts_data/new_alert");
+            this.topics.Add("clean_data/4");
+            this.topics.Add("alerts/state");
+            this.topics.Add("alerts/login/userID");
+            ConnectAndSubscribe();
+        }
+        public static MQTTHandler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MQTTHandler();
+                }
+                return instance;
+            }
+        }
+
+
+
+
+        public void ConnectAndSubscribe()
+        {
+            if (mClient != null && mClient.IsConnected)
+            {
+                Console.WriteLine("ERROR_CONNECTION_EXISTED");
+                return;
+            }
+
+            Console.WriteLine("CONNECTING TO BROKER...");
+            mClient = new MqttClient("mqtt.fmsilva.pt");
+
+            mClient.Connect(Guid.NewGuid().ToString());
+            if (!mClient.IsConnected)
+            {
+                Console.WriteLine("ERROR_CONNECTING_BROKER!");
+                return;
+            }
+            for (int h = 0; h < this.topics.Count; h++)
+            {
+                Console.WriteLine("Connecting to topic number " + h + ":" + this.topics[h]);
+                mClient.Subscribe(new string[] { this.topics[h] }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                Console.WriteLine("Connected sucessfully!");
+            }
+            mClient.MqttMsgPublishReceived += MClient_MqttMsgPublishReceived;
+
+        }
+        public void publishData(string topic, string message)
+        {
+            if (!mClient.IsConnected)
+            {
+                Console.WriteLine("ERROR_BROKER_NOT_CONNECTED");
+                return;
+            }
+            mClient.Publish(topic, Encoding.UTF8.GetBytes(message));
+        }
+        private void MClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+
+            string[] data = e.Topic.Split('/');
+            Console.WriteLine(Encoding.UTF8.GetString(e.Message));
+
+
+
+            if (data[0].Equals("clean_data"))
+            {
+            }
+        }
+
+     
+    }
+}
--- IPL1920-IS-IPLeiriaSmartCampus/File Reader/Program.cs
+++ IPL1920-IS-IPLeiriaSmartCampus/File Reader/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using Models;
 
 namespace File_Reader
 {
@@ -10,6 +12,53 @@ namespace File_Reader
     {
         static void Main(string[] args)
         {
+            byte[] data = File.ReadAllBytes("bin_1");
+
+            for (int i = 0; i < data.Length; i += 24)
+            {
+                // Id
+                byte[] idData = new byte[1];
+                Array.Copy(data, i + 0, idData, 0, 1);
+                byte id = idData[0];
+
+                Console.WriteLine("Id (Raw): " + BitConverter.ToString(idData));
+                Console.WriteLine("Id: " + id);
+
+                // Temperature
+                byte[] temperatureData = new byte[4];
+                Array.Copy(data, i + 4, temperatureData, 0, 4);
+                Single temperature = BitConverter.ToSingle(temperatureData, 0);
+
+                Console.WriteLine("Temperature (Raw): " + BitConverter.ToString(temperatureData));
+                Console.WriteLine("Temperature: " + temperature);
+
+                // Humidity
+                byte[] humidityData = new byte[4];
+                Array.Copy(data, i + 8, humidityData, 0, 4);
+                Single humidity = BitConverter.ToSingle(humidityData, 0);
+
+                Console.WriteLine("Humidity (Raw): " + BitConverter.ToString(humidityData));
+                Console.WriteLine("Humidity: " + humidity);
+
+                // Battery
+                byte[] batteryData = new byte[1];
+                Array.Copy(data, i + 12, batteryData, 0, 1);
+                byte battery = batteryData[0];
+
+                Console.WriteLine("Battery (Raw): " + BitConverter.ToString(batteryData));
+                Console.WriteLine("Battery: " + battery);
+
+                // Timestamp
+                byte[] timestampData = new byte[4];
+                Array.Copy(data, i + 16, timestampData, 0, 4);
+                UInt32 timestamp = BitConverter.ToUInt32(timestampData, 0);
+
+                Console.WriteLine("Timestamp (Raw): " + BitConverter.ToString(timestampData));
+                Console.WriteLine("Timestamp: " + timestamp);
+                Console.WriteLine();
+            }
+
+            Console.Read();
         }
     }
 }
