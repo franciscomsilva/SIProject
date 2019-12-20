fa1a38e394d787a9e4b0ed27d87e61ba9ea74b2b
João Pedro <joaopms@users.noreply.github.com>
2019-12-20 02:01:07 +0000

    Add the route to add sensor readings

---
+++ IPL1920-IS-IPLeiriaSmartCampus/GlobalAPI/Controllers/ReadingsController.cs
@@ -0,0 +1,61 @@
+﻿using GlobalAPI.Filters;
+using GlobalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Routing;
+
+namespace GlobalAPI.Controllers
+{
+    [AuthenticationFilter]
+    public class ReadingsController : ApiController
+    {
+        // POST: api/Readings
+        public IHttpActionResult Post([FromBody]SensorData reading)
+        {
+            Sensor sensor;
+            Location readingLocation;
+            SensorField field;
+            if (reading == null || (sensor = Sensor.GetById(reading.SensorId)) == null) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid sensor"));
+            if (sensor.LocationId == null && (readingLocation = Location.GetById(reading.LocationId)) == null) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid location"));
+            if ((field = SensorField.GetById(reading.FieldId)) == null) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid field"));
+            if (sensor.Id != field.SensorId) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid field and sensor; the sensor doesn't have the specified field"));
+            if (reading.Value == null) return this.Content(HttpStatusCode.BadRequest, new ApiError("Value is required"));
+
+            switch (field.Type)
+            {
+                case "float":
+                    float valueF;
+                    if (!float.TryParse(reading.Value, out valueF)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid value; the provided value can't be casted to the correct type"));
+                    reading.Value = valueF.ToString();
+
+                    if (field.MinValue != null && valueF < float.Parse(field.MinValue)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid value; the provided value is not in the field range"));
+                    if (field.MaxValue != null && valueF > float.Parse(field.MaxValue)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid value; the provided value is not in the field range"));
+
+                    break;
+                case "int":
+                    int valueI;
+                    if (!int.TryParse(reading.Value, out valueI)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid value; the provided value can't be casted to the correct type"));
+                    reading.Value = valueI.ToString();
+
+                    if (field.MinValue != null && valueI < int.Parse(field.MinValue)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid value; the provided value is not in the field range"));
+                    if (field.MaxValue != null && valueI > int.Parse(field.MaxValue)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid value; the provided value is not in the field range"));
+
+                    break;
+                case "bool":
+                    bool valueB;
+                    if (!bool.TryParse(reading.Value, out valueB)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid value; the provided value can't be casted to the correct type"));
+                    reading.Value = valueB.ToString();
+                    break;
+            }
+
+            reading.Insert();
+
+            return Ok(reading);
+        }
+    }
+}
---
+++ IPL1920-IS-IPLeiriaSmartCampus/GlobalAPI/Models/SensorData.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GlobalAPI.Models
+{
+    public class SensorData
+    {
+        [JsonProperty(PropertyName = "sensor_id")]
+        public int SensorId { get; set; }
+
+        [JsonProperty(PropertyName = "location_id")]
+        public int LocationId { get; set; }
+
+        [JsonProperty(PropertyName = "field_id")]
+        public int FieldId { get; set; }
+
+        [JsonProperty(PropertyName = "value")]
+        public string Value { get; set; }
+
+        [JsonProperty(PropertyName = "valid")]
+        public bool Valid { get; set; }
+
+        [JsonProperty(PropertyName = "date")]
+        public string Date { get; set; }
+
+        public void Insert()
+        {
+            using (SqlConnection conn = new SqlConnection(Properties.Settings.Default.DB))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO t_sensor_data (location_id, id_reading, data_value, valid) output INSERTED.ID VALUES (@location_id, @id_reading, @data_value, 1)", conn))
+                {
+                    // TODO Timestamp
+                    this.Date = DateTime.Now.ToString();
+
+                    cmd.Parameters.AddWithValue("@location_id", this.LocationId);
+                    cmd.Parameters.AddWithValue("@id_reading", this.FieldId);
+                    cmd.Parameters.AddWithValue("@data_value", this.Value);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
