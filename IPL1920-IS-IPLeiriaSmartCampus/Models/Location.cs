47e0a0f0cb26efaa2a85e2a2debdd885a72ec5a9
João Pedro <joaopms@users.noreply.github.com>
2019-12-21 21:02:03 +0000

    Revert "Fixed alerts folder"
    This reverts commit 846bd3b8117d03e7fb0bff6467fb308c94f5a196.

---
+++ IPL1920-IS-IPLeiriaSmartCampus/Models/Location.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Location
+    {
+        public int id { get; set; }
+        public string location_name { get; set; }
+        public string gps_coords { get; set; }
+        public Location()
+        {
+
+        }
+        public Location(string name)
+        {
+            this.location_name = name;
+        }
+
+    }
+}
