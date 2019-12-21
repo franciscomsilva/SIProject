da453df1ddc50f54cfbaf43ca83caf190da451fa
João Pedro <joaopms@users.noreply.github.com>
2019-12-21 03:27:45 +0000

    Add the route to get all the triggered user generated alerts

---
+++ IPL1920-IS-IPLeiriaSmartCampus/GlobalAPI/Controllers/AlertsController.cs
@@ -0,0 +1,36 @@
+﻿using GlobalAPI.Filters;
+using GlobalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Routing;
+
+namespace GlobalAPI.Controllers
+{
+    [AuthenticationFilter]
+    public class AlertsController : ApiController
+    {
+        // GET: api/Alerts
+        public IHttpActionResult Get(string startDate = null, string endDate = null)
+        {
+            DateTime _startDateTime, _endDateTime;
+            Nullable<DateTime> startDateTime = null, endDateTime = null;
+
+            if (startDate != null) startDateTime = DateTime.TryParse(startDate, out _startDateTime) ? _startDateTime : (DateTime?)null;
+            if (startDate != null && startDateTime == null) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid start date"));
+            if (startDateTime != null && (startDateTime < DateTime.MinValue || startDateTime > DateTime.MaxValue)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid start date"));
+
+            if (endDate != null) endDateTime = DateTime.TryParse(endDate, out _endDateTime) ? _endDateTime : (DateTime?)null;
+            if (endDate != null && endDateTime == null) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid end date"));
+            if (endDateTime != null && (endDateTime < DateTime.MinValue || endDateTime > DateTime.MaxValue)) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid end date"));
+
+            if ((startDateTime != null && endDateTime != null) && endDateTime < startDateTime) return this.Content(HttpStatusCode.BadRequest, new ApiError("Invalid date range; endDate must be greater than or equal to startDate"));
+
+            return Ok(Alert.GetAll());
+        }
+    }
+}
